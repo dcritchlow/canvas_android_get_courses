5d0bf30938362e1b27eec4d968fb7ee1ed8d8a85
Darin Critchlow <darincritchlow@gmail.com>
2015-06-02 16:00:59 -0600

    moving menu action to the fragment activity

--- app/src/main/java/com/darincritchlow/assignment8/cs3270a8/MainActivity.java
+++ app/src/main/java/com/darincritchlow/assignment8/cs3270a8/MainActivity.java
@@ -47,10 +47,10 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_import) {
-            return true;
-        }
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_import) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
